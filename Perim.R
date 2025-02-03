53d0842ad48e07d796f10b2f2e918651541de9b5
dapuer2 <dapuer2@alumni.uv.es>
2025-02-03 11:18:30 +0100

    Perimetro rectángulo y cambio Pi

--- Perim.R
+++ Perim.R
@@ -3,5 +3,11 @@ PerimetroCuadrado <- function(L){
 }
 
 LongitudCircunferencia <- function(R){
-  return(2*pi*R)
+  return(2*3.14159*R)
+}
+
+# Función para calcular el perímetro de un rectángulo
+calcular_perimetro <- function(longitud, ancho) {
+  perimetro <- 2 * (longitud + ancho)
+  return(perimetro)
 }
